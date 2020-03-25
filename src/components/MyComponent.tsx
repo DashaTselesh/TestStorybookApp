653907529dbfd26af57b2ec21c17294bd7524385
darya_tselesh <d.tselesh@godeltech.com>
2020-03-25 18:50:57 +0300

    initial commit

---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import Slider from '@material-ui/core/Slider';
+
+interface Props {}
+
+const MyComponent: React.FunctionComponent<Props> = () => (
+  <Slider></Slider>
+)
+
+export default MyComponent
